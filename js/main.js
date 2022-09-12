ebf099f361366ac75ec18cc3f4b5d25def2c3f46
Yaser <khanyaser007@gmail.com>
2022-09-12 15:33:32 +0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -5,6 +5,7 @@ const closeEls = document.querySelectorAll('[data-close]');
 const openElems = Array.from(openEls);
 const closeElems = Array.from(closeEls);
 const isVisible = 'is-visible';
+const navLinks = document.querySelectorAll('.web-header-link');
 
 // Open modal
 openElems.forEach((e) => {
@@ -18,6 +19,22 @@ openElems.forEach((e) => {
 closeElems.forEach((e) => {
   e.addEventListener('click', function () {
     this.parentElement.parentElement.classList.remove(isVisible);
+    document.body.style.overflow = 'auto';
+  });
+});
+
+// add event listener on click outside modal to close modal and enable scroll
+document.addEventListener('click', (e) => {
+  if (e.target === document.querySelector('.modal.is-visible')) {
+    document.querySelector('.modal.is-visible').classList.remove(isVisible);
+    document.body.style.overflow = 'auto';
+  }
+});
+
+// add event listener on a tags to close modal, enable scroll, and go to section ID
+navLinks.forEach((elem) => {
+  elem.addEventListener('click', () => {
+    document.querySelector('.modal.is-visible').classList.remove(isVisible);
     document.body.style.overflow = 'auto';
   });
 });
