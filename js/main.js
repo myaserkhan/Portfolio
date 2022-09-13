ad32c05622ab6995511f3074d0d58d834064ce7f
Yaser <khanyaser007@gmail.com>
2022-09-13 13:37:09 +0500

    Add array of project objects

--- js/main.js
+++ js/main.js
@@ -1,3 +1,73 @@
+// array of project objects
+
+const project1 = {
+  title: 'Keeping track of hundreds of components',
+  id: 1,
+  imageSrc:
+    './assets/Desktop Snapshoot Portfolio.png',
+  technologies: ['html', 'CSS', 'Javascript', 'Bootstrap'],
+  description:
+    'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
+  liveLink: 'https://myaserkhan.github.io/Portfolio/',
+  githubLink: 'https://github.com/MYaserKhan/Portfolio',
+};
+const project2 = {
+  title: 'Keeping track of hundreds of components',
+  id: 2,
+  imageSrc:
+    './assets/Desktop Snapshoot Portfolio.png',
+  technologies: ['html', 'SCSS', 'Javascript', 'React'],
+  description:
+    'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
+  liveLink: 'https://myaserkhan.github.io/Portfolio/',
+  githubLink: 'https://github.com/MYaserKhan/Portfolio',
+};
+const project3 = {
+  title: 'Keeping track of hundreds of components',
+  id: 3,
+  imageSrc:
+    './assets/Desktop Snapshoot Portfolio.png',
+  technologies: ['Ruby', 'Jekyll', 'SCSS', 'Javascript'],
+  description:
+    'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
+  liveLink: 'https://myaserkhan.github.io/Portfolio/',
+  githubLink: 'https://github.com/MYaserKhan/Portfolio',
+};
+const project4 = {
+  title: 'Keeping track of hundreds of components',
+  id: 4,
+  imageSrc:
+    './assets/Desktop Snapshoot Portfolio.png',
+  technologies: ['Bootstrap', 'css3', 'Ruby on Rails', 'Ruby'],
+  description:
+    'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
+  liveLink: 'https://myaserkhan.github.io/Portfolio/',
+  githubLink: 'https://github.com/MYaserKhan/Portfolio',
+};
+const project5 = {
+  title: 'Keeping track of hundreds of components',
+  id: 5,
+  imageSrc:
+    './assets/Desktop Snapshoot Portfolio.png',
+  technologies: ['html', 'css', 'React', 'Ruby on Rails', 'Ruby'],
+  description:
+    'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
+  liveLink: 'https://myaserkhan.github.io/Portfolio/',
+  githubLink: 'https://github.com/MYaserKhan/Portfolio',
+};
+const project6 = {
+  title: 'Keeping track of hundreds of components',
+  id: 6,
+  imageSrc:
+    './assets/Desktop Snapshoot Portfolio.png',
+  technologies: ['html', 'css', 'Javascript', 'Webpack'],
+  description:
+    'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
+  liveLink: 'https://myaserkhan.github.io/Portfolio/',
+  githubLink: 'https://github.com/MYaserKhan/Portfolio',
+};
+const projects = [project1, project2, project3, project4, project5, project6];
+
 // Target open and close triggers in html
 
 const openEls = document.querySelectorAll('[data-open]');
@@ -6,6 +76,8 @@ const openElems = Array.from(openEls);
 const closeElems = Array.from(closeEls);
 const isVisible = 'is-visible';
 const navLinks = document.querySelectorAll('.web-header-link');
+
+// Create main page work section html elements
 
 // Open modal
 openElems.forEach((e) => {
