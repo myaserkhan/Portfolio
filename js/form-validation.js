e321bd988d446d4d60d2d85247158604a16b2e6d
Yaser <khanyaser007@gmail.com>
2022-09-14 15:59:11 +0500

    Add functions to show error / success

--- js/form-validation.js
+++ js/form-validation.js
@@ -1,3 +1,19 @@
 // Form validation
 const form = document.querySelector('#signup');
 const emailEl = document.querySelector('#email');
+
+// Functions to show error / success
+const showError = (input, message) => {
+  const formField = input.parentElement;
+  formField.classList.remove('sucess');
+  formField.classList.add('error');
+  const error = formField.querySelector('small');
+  error.textContent = message;
+};
+const showSuccess = (input) => {
+  const formField = input.parentElement;
+  formField.classList.remove('error');
+  formField.classList.add('success');
+  const error = formField.querySelector('small');
+  error.textContent = '';
+};
