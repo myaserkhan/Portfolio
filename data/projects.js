c524f6ed1c1b8501f2052d3cf134d1ea9a4f232a
Yasir Khan <khanyaser007@gmail.com>
2023-04-04 03:03:20 +0500

    Refactor JavaScript code

---
+++ data/projects.js
@@ -0,0 +1,57 @@
+const projects = [
+  {
+    title: 'Path to JS Mastery web dev',
+    description: 'Showcase your expertise to employers with exciting projects where you"ll build real-world applications and have your code reviewed by industry experts.',
+    imageUrl: './images/project_images/jsmastery.png',
+    technologies: ['html', 'CSS', 'Javascript', 'Bootstrap'],
+    liveVersion: 'https://myaserkhan.github.io/Path-to-JavaScript-Mastery/',
+    source: 'https://github.com/MYaserKhan/Path-to-JavaScript-Mastery',
+  },
+
+  {
+    title: 'SPACE TRAVELERS HUB',
+    description: 'This educational project is to build a web application for a company that provides commercial and scientific space travel services. The application will allow users to book rockets and join selected space missions.',
+    imageUrl: './images/project_images/travel.png',
+    technologies: ['React', 'Redux', 'CSS', 'Jest'],
+    liveVersion: 'https://strong-buttercream-e6fb08.netlify.app/',
+    source: 'https://github.com/myaserkhan/React-Redux-Space-Travelers-Hub',
+  },
+
+  {
+    title: 'POKEMON WEB APP',
+    description: 'This educational project is to build a mobile web application to check a list of metrics (numeric values) using React and Redux. An API is selected that provides numeric data about a topic and then build the web app around it.',
+    imageUrl: './images/project_images/poke.png',
+    technologies: ['React', 'Redux', 'Jest', 'JavaScript'],
+    liveVersion: 'https://teal-travesseiro-0a5b57.netlify.app/',
+    source: 'https://github.com/myaserkhan/React-Capstone',
+  },
+
+  {
+    title: 'Math Magicians',
+    description: 'Math Magicians is a React calculator built with JavaScript and JSX. This applications allows you to do your basic computation and calculations easily.',
+    imageUrl: './images/project_images/math.png',
+    technologies: ['Ruby on Rails', 'JavaScript', 'CSS', 'Html'],
+    liveVersion: 'https://636caee9a8434160d14233c5--fastidious-pegasus-fbdfa4.netlify.app/',
+    source: 'https://github.com/myaserkhan/Math-Magicians',
+  },
+
+  {
+    title: 'Prime Shows HD',
+    description: 'This is a web application based on an external API. The external API provides data about TV shows. ',
+    imageUrl: './images/project_images/space.png',
+    technologies: ['JavaScript', 'HTML', 'CSS', 'Jest'],
+    liveVersion: 'https://myaserkhan.github.io/JavaScript-API-based-webapp/dist/',
+    source: 'https://github.com/myaserkhan/JavaScript-API-based-webapp',
+  },
+
+  {
+    title: 'MERN E-Commerce',
+    description: 'In this project I am creating a Single Page App using React regarding basic Calculator and its functionalities.',
+    imageUrl: './images/project_images/amazona.jpg',
+    technologies: ['React', 'Node.js', 'CSS', 'JSX'],
+    liveVersion: 'https://amazona.onrender.com/',
+    source: 'https://github.com/myaserkhan/MERN-E_Commerce',
+  },
+];
+
+export default projects;
